f0ba6f9bf5256cd45be5e9e245f4d8b5312a5961
motoki317 <motoki317@gmail.com>
2019-05-17 13:07:06 +0900

    Added cities in countries

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -17,6 +17,18 @@
     <p>
       <router-link to="/countries">Back to countries list</router-link>
     </p>
+    <div>
+      <h3>{{ country.name }}の街</h3>
+      <div class="cities-container">
+        <div v-for="city in cities" :key="city.name">
+          <div class="city">
+            <router-link :to="'/city/' + city.name">{{
+              city.name
+            }}</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,7 +38,8 @@ export default {
   name: "Country",
   data() {
     return {
-      country: null
+      country: null,
+      cities: null
     };
   },
   mounted() {
@@ -34,6 +47,14 @@ export default {
       .get("/api/country/" + this.$route.params.countryName)
       .then(response => {
         this.country = response.data.country;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    axios
+      .get("/api/citiesInCountry/" + this.$route.params.countryName)
+      .then(response => {
+        this.cities = response.data.cities;
       })
       .catch(error => {
         console.error(error);
